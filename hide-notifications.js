28dfd6ac66a7628b1cfb016f50aebb4189c68f71
manuell18 <emmmanuelmilimowams@gmail.com>
2023-11-20 21:29:47 +0300

    third commit

---
+++ hide-notifications.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($) {
+    // Replace '.notice' with the appropriate class/ID of the notifications
+    $('.notice').hide(); // This will hide all notifications with class 'notice'
+});
